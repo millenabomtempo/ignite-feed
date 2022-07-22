eb94db49ea4a623b778d8983dfe5151131ab2eaf
Millena Bomtempo <millena.bomtempo@gmail.com>
2022-07-22 10:43:51 -0300

    creating and styling sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { Header } from "./components/Header"
 
+import styles from './App.module.css'
 import './global.css'
+import { Sidebar } from "./components/Sidebar"
 
 function App() {
 
   return (
     <>
       <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>main</main>
+      </div>
     </>
   )
 }
