48fe56890ab21752074006a0c21e4efb4a700b69
Millena Bomtempo <millena.bomtempo@gmail.com>
2022-07-22 13:33:16 -0300

    create component Avatar, style it and apply where needed

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar'
 
 import styles from './Sidebar.module.css'
 
@@ -9,10 +10,7 @@ export function Sidebar() {
         className={styles.cover}
        src="https://images.unsplash.com/photo-1643966392439-bbe3992e5770?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" />
       <div className={styles.profile}>
-        <img 
-          className={styles.avatar}
-          src="https://github.com/millenabomtempo.png" 
-        />
+        <Avatar src="https://github.com/millenabomtempo.png" />
         <strong>Millena Bomtempo</strong>
         <span>Web developer</span>
       </div>
