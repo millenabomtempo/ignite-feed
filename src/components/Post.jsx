98524e3c98c25507028b12735a110cb47681be89
Millena Bomtempo <millena.bomtempo@gmail.com>
2022-07-22 11:53:08 -0300

    create form section on post component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -17,14 +17,25 @@ export function Post() {
         <time title='22 de julho às 10:30' dateTime='2022-07-22 10:30'>Públicado há 1h</time>
       </header>
       <div className={styles.content}>
-          <p>Fala galeraa 👋</p>
+        <p>Fala galeraa 👋</p>
 
-          <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
 
-          <p>👉 <a href="">jane.design/doctorcare</a></p>
+        <p>👉 <a href="">jane.design/doctorcare</a></p>
 
-          <p><a href="">#novoprojeto</a> <a href="">#nlw</a> <a href="">#rocketseat</a></p>
-        </div>
+        <p><a href="">#novoprojeto</a> <a href="">#nlw</a> <a href="">#rocketseat</a></p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea 
+          placeholder='Deixe um comentário'
+        />
+
+        <footer>
+          <button type='submit'>Publicar</button>
+        </footer>
+      </form>
     </article>
   )
 }
