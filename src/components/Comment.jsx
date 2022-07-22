d5750964507103be3419e05d8ae6cf45deb993d1
Millena Bomtempo <millena.bomtempo@gmail.com>
2022-07-22 12:44:41 -0300

    create comment component and add it to post component

---
+++ src/components/Comment.jsx
@@ -0,0 +1,33 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+     <img 
+        className={styles.avatar}
+        src="https://github.com/millenabomtempo.png" 
+      />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Millena Bomtempo</strong>
+              <time title='22 de julho às 10:30' dateTime='2022-07-22 10:30'>Cerca de 1h atrás</time>
+            </div>
+            <button title='Deletar comentário'>
+              <Trash size={20}/>
+            </button>
+          </header>
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
